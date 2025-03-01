22786bc5a268b83cbd0defdd802704343824682a
euri10 <benoit.barthelet@gmail.com>
2025-03-01 11:37:05 +0100

    add test for custom jinja2 environment

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+from jinja2 import Environment, FileSystemLoader
 from litestar import Litestar, Response, get, websocket_listener
 from litestar.contrib.jinja import JinjaTemplateEngine
 from litestar.response import Template
@@ -17,7 +18,7 @@ def anyio_backend() -> str:
 
 @pytest.mark.anyio
 @pytest.mark.parametrize("ws_reload_path", ["/__litestar__", "/custom_reload_path"])
-async def test_plugin(tmp_path: Path, ws_reload_path: str) -> None:
+async def test_plugin_default_engine(tmp_path: Path, ws_reload_path: str) -> None:
     templates_dir = tmp_path / "templates"
     templates_dir.mkdir()
     index_jinja = templates_dir / "index.html"
@@ -25,6 +26,51 @@ async def test_plugin(tmp_path: Path, ws_reload_path: str) -> None:
 
     template_config = TemplateConfig(
         engine=JinjaTemplateEngine, directory=templates_dir
+    )
+
+    @get("/")
+    async def render_page() -> Response:
+        page_content = "omg this is a html page"
+        return Template("index.html", context={"page_content": page_content})
+
+    hotreload_plugin = HotReloadPlugin(
+        template_config=template_config,
+        watch_paths=[templates_dir],
+        reconnect_interval=0.5,
+        ws_reload_path=ws_reload_path,
+    )
+    app = Litestar(
+        route_handlers=[
+            render_page,
+        ],
+        debug=True,
+        template_config=template_config,
+        plugins=[hotreload_plugin],
+    )
+
+    async with AsyncTestClient(app) as client:
+        response = await client.get("/")
+        assert "window.location.reload()" in response.text
+
+        with await client.websocket_connect(ws_reload_path) as ws:
+            index_jinja.write_text("modified")
+
+            assert ws.receive_text() == "reload"
+        response = await client.get("/")
+        assert "modified" in response.text
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize("ws_reload_path", ["/__litestar__", "/custom_reload_path"])
+async def test_plugin_non_default_engine(tmp_path: Path, ws_reload_path: str) -> None:
+    templates_dir = tmp_path / "templates"
+    templates_dir.mkdir()
+    index_jinja = templates_dir / "index.html"
+    index_jinja.write_text("<body>{{ page_content }}</body>")
+
+    environment = Environment(loader=FileSystemLoader(templates_dir), autoescape=True)
+    template_config = TemplateConfig(
+        instance=JinjaTemplateEngine.from_environment(environment),
     )
 
     @get("/")
